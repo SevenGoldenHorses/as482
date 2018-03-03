79487d020ccc3507013caf88ba5235033af5b963
MiterX <you@example.com>
2018-03-04 00:37:23 +0100

    make it better

---
+++ Key_secret.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'tT2zkUR7kd1avZMLZNF6AKEY6',
+  consumer_secret:      '2IAKluug7Is0YDVVsY7EMMAvau2HjhlfnPXu12bfH090neonT9',
+  access_token:         '133522065-yoNvs60KKMlXZQRYqJ6vpaclzlJNiLl8hmoih8yP',
+  access_token_secret:  '8rNPeWPc9nj3WnKZEW2erT1mNWR3DISKqM5b2wHuFMatc',
+  // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
