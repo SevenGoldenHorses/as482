e1d0c694ec460d67b525c5b22af895fa74806bcd
3d4K9ZlAUUic <33258239+SevenGoldenHorses@users.noreply.github.com>
2018-05-12 18:11:33 +0200

    cambiati token

--- Key_secret.js
+++ Key_secret.js
@@ -1,7 +1,7 @@
 module.exports = {
   consumer_key:         'tT2zkUR7kd1avZMLZNF6AKEY6',
   consumer_secret:      '2IAKluug7Is0YDVVsY7EMMAvau2HjhlfnPXu12bfH090neonT9',
-  access_token:         '133522065-yoNvs60KKMlXZQRYqJ6vpaclzlJNiLl8hmoih8yP',
-  access_token_secret:  '8rNPeWPc9nj3WnKZEW2erT1mNWR3DISKqM5b2wHuFMatc',
+  access_token:         '3h7c0ah7b9xVIfB0A0KYyd07NVm181xe76oDqYzQ',
+  access_token_secret:  ' BKhPS0vPzfB0sNMs2589Ap7JQz5DiY6Zp1OgBiSv3Yu8C',
   // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
